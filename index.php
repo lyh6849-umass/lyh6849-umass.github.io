1be5a2798dbc17725fbdf680fa544d77a7f5c175
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2020-03-14 12:19:42 -0400

    1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link href = "index.css" type = "text/css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+</head>
+ 
+<body>
+hellow world;
+    <script
+    src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <script src='index.js'></script> 
+
+ 
+    </body>
+</html>
+ 
+
