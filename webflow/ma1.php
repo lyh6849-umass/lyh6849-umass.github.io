a00de599bcf1fab42b0fe640320b48bf47e54bcd
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2020-05-03 13:50:46 -0400

    1

--- webflow/ma1.php
+++ webflow/ma1.php
@@ -24,13 +24,16 @@
     <form id="email-form" name="email-form" data-name="Email Form" method="post" class="form" action="ma2.php">
       <div class="c3 q1">
         <div class="b1">
-          <h1 class="heading">Questionnaire Number</h1><input type="text" class="text-field w-input" maxlength="256" name="q1" data-name="q1" placeholder="Number Here" id="q" required=""><a href="#" class="button-2 w-button q1a1">Next</a></div>
+<?php
+$i= uniqid();
+?>
+          <h1 class="heading">Questionnaire Number</h1><input type="text" class="text-field w-input" maxlength="256" name="q1" <?php echo "value=\"".$i."\"";?>data-name="q1" placeholder="Number Here" id="q" required=""><a href="#" class="button-2 w-button q1a1">Next</a></div>
           
         </div>
       <div class="c3">
         <div class="b1">
           <h1 class="heading">VISIT DIAGNOSIS</h1>
-          <div class="c4 input_list" id><input type="text"  list="list2" class="text-field w-input" maxlength="256" name="q2_1" data-name="q2" placeholder="Search Here" id="q-2" required=""></div>
+          <div class="c4 input_list" id><input type="text" list="list2" class="text-field w-input" maxlength="256" name="q2_1" data-name="q2"  placeholder="Search Here" id="q-2" required=""></div>
           <div class="div-block-5"><a href="#" class="button-2 w-button plus">+</a><input type="submit" value="Submit" data-wait="Please wait..." class="submit-button w-button"></div>
         </div>
       </div>
