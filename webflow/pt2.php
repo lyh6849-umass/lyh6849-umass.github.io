55af19e15d507b960701688e72de36207c3c3fb9
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2020-05-03 12:45:44 -0400

    1

--- webflow/pt2.php
+++ webflow/pt2.php
@@ -113,7 +113,7 @@ if($cc!==""){
 ?>
       <div class="c3">
         <div class="b1">
-          <p class="paragraph-2">You have answered all question. Answers are going to be reviewed after Dr. Lee finishes encounter with previous patient. If you need <span class="text-span">EMERGENCY </span>care, please notify medical assistant who&#x27;s at the counter for help. </p><input type="submit" value="Submit" class="sbm w-button"></div>
+        <p class="paragraph-2">You&#x27;ve answered all questions. Answers will be reviewed at your appointment time. For <span class="text-span">EMERGENCY </span>services, please call 911.</p><input type="submit" value="Submit" class="sbm w-button"></div>
       </div></form>
 
 
