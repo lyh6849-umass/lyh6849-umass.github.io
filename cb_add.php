771969f2487f49669b0b51dedcc0ebe5d4af58e8
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2020-05-07 22:13:37 -0400

    1

--- cb_add.php
+++ cb_add.php
@@ -100,7 +100,7 @@ for ($u=0;$u<=$n;$u++){
                     if ($conn->query($sql) === TRUE) {echo "";} else {echo "Error: " . $sql . "<br>" . $conn->error;}
                     $t = ucwords($t);
                     echo "T is here and it is: ".$t."<br>";
-                    $sql = "INSERT INTO cc_db (q_id, visit_diagnosis) VALUES ('$s','$t') ON DUPLICATE KEY UPDATE q_id='$s', visit_diagnosis='$t';";  
+                    $sql = "INSERT INTO cc_db (q_id, visit_diagnosis) VALUES ('$s','$t') ON DUPLICATE KEY UPDATE q_id='$s';";  
                     if ($conn->query($sql) === TRUE) {echo "";} else {echo "Error: " . $sql . "<br>" . $conn->error;}
                 
                 }                
