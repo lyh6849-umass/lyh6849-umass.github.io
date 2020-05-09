c0c8ae195f57dea0cfa31d20f92c8e7833c4886f
lyh6849 <58195341+lyh6849@users.noreply.github.com>
2020-05-08 21:16:44 -0400

    1

--- cc_db.php
+++ cc_db.php
@@ -14,9 +14,22 @@ $r=$conn->query($sql);
 if($r->num_rows>0){
     echo "[cc_db]<br><table><th>id</th><th>q_id</th><th>visit_diagnosis</th></tr>";
     while ($row=$r->fetch_assoc()){
-      echo "<tr><td>".$row['id']."</td><td>".$row['q_id']."</td><td>".$row['visit_diagnosis']."</span></td></tr>";
+      echo "<tr><td>".$row['id']."</td><td>".$row['q_id']."</td><td>".$row['visit_diagnosis']."</td></tr>";
     }
     echo"</table>";
 }
+
+if ($conn->query($sql) === TRUE) {echo "";} else {echo "Error: " . $sql . "<br>" . $conn->error;};
+
+
+$sql = "SELECT * FROM answer_db;";
+if($r->num_rows>0){
+  echo "[cc_db]<br><table><th>answer_id</th><th>answer_value</th><th>cc_id</th><th>answer_type</th></tr>";
+  while ($row=$r->fetch_assoc()){
+    echo "<tr><td>".$row['answer_id']."</td><td>".$row['answer_value']."</td><td>".$row['cc_id']."</td><td>".$row['answer_type']."</td></tr>";
+  }
+  echo"</table>";
+}
+if ($conn->query($sql) === TRUE) {echo "";} else {echo "Error: " . $sql . "<br>" . $conn->error;};
 
 ?>
